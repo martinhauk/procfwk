f81b258691215f3ab19c04c9a448fc71e262285d
Martin Hauk <m.hauk@oh22.net>
2023-11-30 10:09:05 +0100

    add explicit namespace, add class and namespace to program cs

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,7 +1,16 @@
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .Build();
+namespace mrpaulandrew.azure.procfwk
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWebApplication()
+                .Build();
 
-host.Run();
+            host.Run();
+        }
+    }
+}
